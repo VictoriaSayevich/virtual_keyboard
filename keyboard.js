1722572cfea604d5594a614f4777b0ae91a94e46
Victoria Sayevich <victoriasayevich.gmail.com>
2019-11-06 23:54:49 +0300

    feat: keyboard.js create Keyboard. Add events - CapsLook

--- keyboard.js
+++ keyboard.js
@@ -1,32 +1,36 @@
 const Keyboard = {
     elements: {
-        keysContainer: null,
-        keysRow: [],
+        textarea: null,
+        container: null,
+        rows: [],
         keys: []
     },
 
-    content: {
-        rows: [{
-                eng: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace'],
-                ru: ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace']
-            },
-            {
-                eng: ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'sk', 'sk', 'spec'],
-                ru: ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'spec']
-            },
-            {
-                eng: ['CapsLk', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', 'SP,', 'Enter'],
-                ru: ['CapsLk', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'SP,', 'Enter']
-            },
-            {
-                eng: ['Sift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift'],
-                ru: ['Sift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '/', 'Shift']
-            },
-            {
-                eng: ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '<', '<'],
-                ru: ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '<', '<', '<']
-            }
+    keyboardContent: {
+        ru: [
+            ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace'],
+            ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'],
+            ['CapsLk', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', ',', 'Enter'],
+            ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', '▲', 'Shift'],
+            ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►']
+        ],
+        en: [
+            ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '+', 'Backspace'],
+            ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}'],
+            ['CapsLk', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'Enter'],
+            ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
+            ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '◄', '▼', '►']
         ]
+    },
+
+    properties: {
+        value: "",
+        capsLook: false,
+        shift: false,
+        language: {
+            ru: true,
+            en: false
+        }
     },
 
     _createElement(my_tag, my_class = '') {
@@ -35,59 +39,146 @@ const Keyboard = {
         return element;
     },
 
+    _main() {
+        this._createKeyboard();
+        this._events();
+    },
+
     _createKeyboard() {
-        //Create CONTAINER for keyboard
-        this.elements.keysContainer = this._createElement('div', 'keysContainer');
+        this.elements.textarea = this._createElement('textarea', 'result');
+        this.elements.textarea.setAttribute('cols', '80');
+        this.elements.textarea.setAttribute('rows', '20');
+        document.body.appendChild(this.elements.textarea);
+        this.elements.container = this._createElement('div', 'containerKeyboard');
+        document.body.appendChild(this.elements.container);
 
-        document.querySelector('body').appendChild(this.elements.keysContainer);
-
-        //Create ROWS
-        // for (let i = 0; i < this.content.rows.length; i++) {
-        //     const row = this._createElement('div', 'keysRow');
-        //     this.elements.keysContainer.appendChild(row);
-        // }
-
-        //Create KEYS
-
-        for (let i = 0; i < this.content.rows.length; i++) {
-            const row = this._createElement('div', 'keysRow');
-            this.elements.keysContainer.appendChild(row);
-            for (let j = 0; j < this.content.rows[i].eng.length; j++) {
+        const createIcon = (iconName) => {
+            return `<i class="${iconName}"></i>`;
+        };
+        for (let i = 0; i < this.keyboardContent.en.length; i++) {
+            const row = this._createElement('div', 'rowKeyboard');
+            this.elements.container.appendChild(row);
+            for (let j = 0; j < this.keyboardContent.en[i].length; j++) {
                 const key = this._createElement('div', 'key');
-                key.innerHTML = '<span>' + this.content.rows[i].eng[j] + '</span>';
-                key.classList.add('eng');
+                let text_ru = this.keyboardContent.ru[i][j];
+                let text_en = this.keyboardContent.en[i][j];
+                const span_ru = this._createElement('span', 'ru');
+                const span_en = this._createElement('span', 'en');
+                switch (this.keyboardContent.en[i][j]) {
+                    case "Tab":
+                        key.classList.add('special--tab');
+                        break;
+                    case "CapsLk":
+                        key.classList.add('special--capsLook');
+                        break;
+                    case '◄':
+                        key.classList.add("special--arrow_left");
+                        // text_ru = text_en = createIcon("fas fa-caret-square-left");
+                        break;
+                    case "▼":
+                        key.classList.add("special--arrow_down");
+                        // text_ru = text_en = createIcon("fas fa-caret-square-down");
+                        break;
+                    case "►":
+                        key.classList.add("special--arrow_right");
+                        // text_ru = text_en = createIcon("fas fa-caret-square-right");
+                        break;
+                    case "▲":
+                        key.classList.add("special--arrow_up");
+                        // text_ru = text_en = createIcon("fas fa-caret-square-up");
+                        break;
+                    case "Space":
+                        key.classList.add("special--space");
+                        break;
+                    case "Enter":
+                        key.classList.add("special--enter");
+                        break;
+                    case "Alt":
+                        key.classList.add("special--Alt");
+                        break;
+                    case "Shift":
+                        key.classList.add("special--Shift");
+                        break;
+                    case "Ctrl":
+                        key.classList.add("special--Ctrl");
+                        break;
+                    case "Backspace":
+                        key.classList.add("special--Backspace");
+                        break;
+                    case "Win":
+                        key.classList.add("special--Win");
+                        break;
+                    case "Shift":
+                        key.classList.add("special--Shift");
+                        break;
+                    default:
+                        key.classList.add("letter");
+                        break;
+                }
                 row.appendChild(key);
+                (key.appendChild(span_ru)).appendChild(document.createTextNode(text_ru));
+                (key.appendChild(span_en)).appendChild(document.createTextNode(text_en));
             }
         }
 
-        // const rows = document.querySelectorAll('.keysRow');
-        // rows.forEach(row => {
-        //     for (let i = 0; i < rows.eng.length; i++) {
-        //         const key = this._createElement('div', 'key');
-        //         row.appendChild(key);
-        //     }
-        // });
     },
 
+    _events() {
+        document.querySelector('.containerKeyboard').addEventListener('mouseover', this._hover);
+        document.querySelector('.containerKeyboard').addEventListener('click', this._click);
+    },
 
-    _createKeys() {
-
-        for (let i = 0; i < keyRowContent.rows.length; i++) {
-            const rowContainer = [];
-            let row = this._createElement('div', 'keysRow');
-            rowContainer.push(row);
-            keyRowContent.rows[i].eng.forEach(symbol => {
-                const key = this._createElement('div', 'key');
-                key.innerHTML = '<span>' + symbol + '</span>';
-                key.classList.add('engl');
-                rowContainer[i].appendChild(key);
+    _toCapsLook() {
+        const spanKeys = document.querySelectorAll('.letter');
+        console.log(spanKeys);
+        if (this.properties.capsLook) {
+            document.querySelector('.special--capsLook').style.background = 'rgb(255, 147, 147)';
+            spanKeys.forEach(key => {
+                key.style.textTransform = "lowercase";
             });
+            this.properties.capsLook = false;
+        } else {
+            document.querySelector('.special--capsLook').style.background = "red";
+            spanKeys.forEach(key => {
+                key.style.textTransform = "uppercase";
+            });
+            this.properties.capsLook = true;
         }
-        return rowContainer;
+    },
+
+    _toBackspace() {
+
+    },
+
+    _click() {
+        const target = event.target;
+        if (target.classList.value.indexOf('key') >= 0 || target.tagName === 'SPAN') {
+            if (target.classList.value.indexOf('special') >= 0 || target.parentNode.classList.value.indexOf('special') >= 0) {
+                const spanKeys = document.querySelectorAll('span');
+                if (target.classList.value.indexOf('capsLook') >= 0 || target.parentNode.classList.value.indexOf('capsLook') >= 0) {
+                    //not a function!!!???? typeError
+                    Keyboard._toCapsLook();
+                }
+                if (target.classList.value.indexOf('backspace') >= 0 || target.parentNode.classList.value.indexOf('backspace') >= 0) {
+                    textarea.value = textarea.value.substring(0, textarea.values.length - 1);;
+                }
+            } else {
+                if (event.target.className === 'key') Keyboard.elements.textarea.value += event.target.lastChild.innerHTML;
+                else Keyboard.elements.textarea.value += event.target.innerHTML;
+            }
+
+        } else {
+            if (event.target.className === 'key') Keyboard.elements.textarea.value += event.target.lastChild.innerHTML;
+            else Keyboard.elements.textarea.value += event.target.innerHTML;
+        }
+
+    },
+
+    _hover() {
+        event.target.style.cursor = "pointer";
     }
 }
 
-
 window.addEventListener("DOMContentLoaded", function() {
-    Keyboard._createKeyboard();
+    Keyboard._main();
 });
